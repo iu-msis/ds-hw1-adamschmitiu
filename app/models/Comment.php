9cb42ef0e773494c9be79a7dc9861d79e18a535a
adamschmitiu <42544767+adamschmitiu@users.noreply.github.com>
2018-10-09 19:16:27 -0400

    changed it up

--- app/models/Comment.php
+++ app/models/Comment.php
@@ -10,6 +10,31 @@ class Comment
 
     $this->comment = intval($row['comment']);
 }
+public static function getWorkByTaskId() {
+  //public static function fetchAll() {
+    // 1. Connect to the database
+    $db = new PDO(DB_SERVER, DB_USER, DB_PW);
+
+    // 2. Prepare the query
+    $sql = 'SELECT * FROM Comments';
+
+    $statement = $db->prepare($sql);
+
+    // 3. Run the query
+    $success = $statement->execute();
+  // 4. Handle the results
+  $arr = [];
+  while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+    // 4.a. For each row, make a new work object
+    $commentItem =  new Comment($row);
+
+    array_push($arr, $commentItem);
+  }
+
+  // 4.b. return the array of work objects
+
+  return $arr;
+}
 
 public function create() {
   $db = new PDO(DB_SERVER, DB_USER, DB_PW);
@@ -22,31 +47,7 @@ public function create() {
   ]);
   $this->$id = $db->lastInsertId();
 }
-  public static function getWorkByTaskId() {
-    //public static function fetchAll() {
-      // 1. Connect to the database
-      $db = new PDO(DB_SERVER, DB_USER, DB_PW);
 
-      // 2. Prepare the query
-      $sql = 'SELECT * FROM Comments';
-
-      $statement = $db->prepare($sql);
-
-      // 3. Run the query
-      $success = $statement->execute();
-    // 4. Handle the results
-    $arr = [];
-    while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
-      // 4.a. For each row, make a new work object
-      $commentItem =  new Comment($row);
-
-      array_push($arr, $commentItem);
-    }
-
-    // 4.b. return the array of work objects
-
-    return $arr;
-  }
 
 
 }
