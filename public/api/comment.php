1afb1484e52356e517c0730c19e1e61c96880de9
adamschmitiu <42544767+adamschmitiu@users.noreply.github.com>
2018-10-09 15:53:54 -0400

    help 2

--- public/api/comment.php
+++ public/api/comment.php
@@ -7,11 +7,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   exit;
 }
 
-//$taskId = intval($_GET['taskId'] ?? 0);
+$taskId = intval($_GET['taskId'] ?? 0);
 
-//if ($taskId < 1) {
-  //throw new Exception('Invalid Task ID');
-//}
+if ($taskId < 1) {
+  throw new Exception('Invalid Task ID');
+}
+
 
 // 1. Go to the database and get all work associated with the $taskId
 // $comments = Comment::fetchAll()
